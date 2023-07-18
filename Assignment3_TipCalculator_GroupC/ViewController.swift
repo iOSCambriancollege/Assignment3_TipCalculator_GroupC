a928237b78c84524ecb8fbf49779b3d9c3768869
majidpawar9 <majidpawar9@gmail.com>
2023-07-18 19:10:26 -0400

    added functionality to slider

--- Assignment3_TipCalculator_GroupC/ViewController.swift
+++ Assignment3_TipCalculator_GroupC/ViewController.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipIndicator: UILabel!
+    @IBAction func tipSlider(_ sender: UISlider) {
+        tipIndicator.text = "The current tip is \(Int(sender.value))%"
+    }
+    
     @IBOutlet weak var userAmountInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
-
 
 }
 
