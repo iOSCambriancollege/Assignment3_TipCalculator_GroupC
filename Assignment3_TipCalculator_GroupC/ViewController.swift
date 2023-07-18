6bab2890439f8d83e28c17ff7a64bc1d33b8dd1c
majidpawar9 <majidpawar9@gmail.com>
2023-07-18 18:22:16 -0400

    Added title, input for bill amount and a slider

--- Assignment3_TipCalculator_GroupC/ViewController.swift
+++ Assignment3_TipCalculator_GroupC/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userAmountInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
